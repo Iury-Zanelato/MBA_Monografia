14df2a344b2ffb19c668a4f093b131a0b499d041
iury <iury.ribeirozanelato@gmail.com>
2024-08-13 20:55:23 -0300

    adição de alimentadores

---
+++ examples/feeders/123Bus/123Bus.py
@@ -0,0 +1,13 @@
+import os
+import py_dss_tools
+
+dss_file = r"C:\IuryZanelato\GitHub\py-dss-tools\examples\feeders\123Bus\IEEE123Master.dss"
+
+st = py_dss_tools.CreateStudy.power_flow("Test 1", dss_file)
+
+st.dss.text("New EnergyMeter.Feeder Line.L115 1")
+st.dss.text("solve")
+
+print(os.path.dirname(os.path.abspath(__file__)))
+
+st.view.voltage_profile()
